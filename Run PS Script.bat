f064b4170f538cd684580d2406e5fc6303acdd65
Yohanna Gadelrab <Yohanna@users.noreply.github.com>
2014-12-25 18:59:59 -0500

    PAUSE the script if there was an error.

--- Run PS Script.bat
+++ Run PS Script.bat
@@ -4,4 +4,8 @@ SET ThisScriptDirectory=%~dp0
 SET PowerShellScriptPath=%ThisScriptDirectory%RegJump.ps1
 PowerShell -NoProfile -ExecutionPolicy Bypass -Command "& '%PowerShellScriptPath%'";
 
+tasklist /FI "IMAGENAME eq regedit.exe" 2>NUL | find /I /N "regedit.exe">NUL
+
+:: PAUSE if registry editor didn't start; i.e. there was an error with the key
+if "%ERRORLEVEL%"=="1" PAUSE
 
